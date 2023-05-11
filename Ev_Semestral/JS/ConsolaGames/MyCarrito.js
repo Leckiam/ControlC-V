978b535b06b042a07ffeb8c9dee854dc3d0070c4
Maikel Andres Cisternas Mercado <107263990+Leckiam@users.noreply.github.com>
2023-05-11 02:04:06 -0400

    Leckiam (#9)
    * Inclusion de una API (RAWG), la cual se implemento para reemplazar las imagenes que se ven en consolas. Ademas de algunos cambios al css, sin olvidar las nuevas adiciones de JS para la vista de carrito-consolas.
    
    * Inclusion efectiva de una API en el proyecto para las vistas de las 3 Consolas, ademas de validaciones para Login y Register. Todo esto se puede ver desde las files o archivos .js
    
    * Ultimos cambios para la Ev_2, añadimiento del boton eliminar del carro, no se han subido los cambios y js para intentar trabajar con el js del search, se reorganizaron las carpetas, para que quede mas bonito.

--- Ev_Semestral/JS/ConsolaGames/MyCarrito.js
+++ Ev_Semestral/JS/ConsolaGames/MyCarrito.js
@@ -1,31 +1,43 @@
 if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+    document.addEventListener('DOMContentLoaded', function(){
+        timeReady()
+    })
 } else {
-    ready();
+    timeReady();
 }
-const seeCarrito = document.getElementsByClassName('carrito')[0];
+function timeReady(){
+    let segundos = 0;
+    let tiempo = setInterval(function (){
+        segundos++;
+        if (parseFloat(segundos/5)==parseInt(segundos/5)) {
+            ready();
+        } else if (segundos==30) {
+            clearInterval(tiempo);
+        }
+    }, 1000);
+}
 
 function ready() {
+    /* Borrar o quitar alguno de los items (los juegos), del carrito */
+    
     /* Pagar carrito de PS, XB o NT*/
-    let btnPagar = document.getElementsByClassName('btn-pagar')
-    for (let i = 0; i < btnPagar.length; i++) {
-        let buttonP = btnPagar[i];
-        buttonP.addEventListener('click', function () {
-            alert('Pagando...')
-        });
-    }
+    let btnPagar = document.getElementsByClassName('btn-pagar')[0]
+    btnPagar.addEventListener('click', function () {
+        location.href = "Index.html";
+    });
     /* Actualizar precio de juegos en carrito de las consolas PS, XB y NT*/
     let getDiaTotal = document.getElementsByClassName('carrito-dias-total')[0];
-    getDiaTotal.addEventListener("input", function () {
+    getDiaTotal.addEventListener("blur", function () {
         limitTimeDay(getDiaTotal);
         actualizarTotalCarrito();
     });
     /* Agregar carrito en PS, XB y en NT*/
-    let botonesAgregarAlCarrito = document.getElementsByClassName('boton-item');
-    for (let i = 0; i < botonesAgregarAlCarrito.length; i++) {
-        let button = botonesAgregarAlCarrito[i];
-        button.addEventListener('click', agregarAlCarritoClicked);
+    let btnsAddCarrito = document.getElementsByClassName('boton-item');
+    for (let i = 0; i < btnsAddCarrito.length; i++) {
+        let btnAdd = btnsAddCarrito[i];
+        btnAdd.addEventListener('click', agregarAlCarritoClicked);
     }
+    cantGame = document.getElementsByClassName('item').length;
 }
 
 function limitTimeDay(inputDay) {
@@ -71,27 +83,52 @@ function obtenerPrecio(consola) {
     return precio = listaPrice[pos];
 }
 function agregarAlCarritoClicked(event) {
+    let carrito = document.getElementById('carrito');
+    carrito.style.display = "block";
     let button = event.target;
     let item = button.parentElement;
     let titulo = item.getElementsByClassName('titulo-item')[0].innerText;
-    const genConsole = item.getElementsByClassName('consola-item')[0];
+    let genConsole = item.getElementsByClassName('consola-item')[0];
     let precio = obtenerPrecio(genConsole);
     let imagenSrc = item.getElementsByClassName('img-item')[0].src;
     console.log(imagenSrc);
 
     agregarItemAlCarrito(titulo, precio, imagenSrc, genConsole);
 }
+function eliminarItemCarrito(event){
+    var buttonClicked = event.target;
+    let divItem = buttonClicked.parentElement.parentElement.parentElement;
+    divItem.remove();
+    actualizarTotalCarrito();
+
+    ocultarCarrito();
+}
+
+function ocultarCarrito(){
+    var carritoItems = document.getElementsByClassName('carrito-items')[0];
+    if(carritoItems.childElementCount==0){
+        var carrito = document.getElementsByClassName('carrito')[0];
+        carrito.style.display = 'none';
+    }
+}
 function agregarItemAlCarrito(titulo, precio, imagenSrc, genConsole) {
     let item = document.createElement('div');
     item.classList.add = ('item');
     let initConsole = genConsole.innerText.substr(0, 1);
+    if (initConsole=='P') {
+        initConsole='PS'
+    } else if (initConsole=='N') {
+        initConsole='NT'
+    } else {
+        initConsole='XB'
+    }
     let itemsCarrito = document.getElementsByClassName('carrito-items')[0];
     let consoleSelected = genConsole.value;
 
     let nombresItemsCarrito = itemsCarrito.getElementsByClassName('carrito-item-titulo');
     for (let i = 0; i < nombresItemsCarrito.length; i++) {
         if (nombresItemsCarrito[i].innerText == titulo) {
-            alert("El item ya se encuentra en el carrito");
+            alert(titulo+" ya se encuentra en el carrito");
             return;
         }
     }
@@ -100,7 +137,7 @@ function agregarItemAlCarrito(titulo, precio, imagenSrc, genConsole) {
         <div class="carrito-item">
             <div class="carrito-item-detalles backgroud${initConsole}">
                 <span class="carrito-item-titulo">${titulo}</span>
-                <img src="${imagenSrc}" alt="">
+                <img src="${imagenSrc}" alt="" height="304">
                 <div class="fila">
                     <strong>Precio/dia: </strong>
                     <span class="carrito-item-precio">
@@ -112,29 +149,30 @@ function agregarItemAlCarrito(titulo, precio, imagenSrc, genConsole) {
                     ${consoleSelected}
                     </span>
                 </div>
+                <button style="background: #000; color: #fff;" class="btn-borrar">Borrar</button>
             </div>
         </div>
     `;
     item.innerHTML = itemCarritoContenido;
     itemsCarrito.append(item);
+    
+    let btnDelete = item.getElementsByClassName('btn-borrar')[0];
+    btnDelete.addEventListener('click',eliminarItemCarrito);
 
     actualizarTotalCarrito();
 }
 function actualizarTotalCarrito() {
     let carritoContenedor = document.getElementsByClassName('carrito')[0];
-    let carritoItems = carritoContenedor.getElementsByClassName('carrito-item');
+    let carritoItem = carritoContenedor.getElementsByClassName('carrito-item');
     let total = 0;
-    for (let i = 0; i < carritoItems.length; i++) {
-        let item = carritoItems[i];
+    for (let i = 0; i < carritoItem.length; i++) {
+        let item = carritoItem[i];
         let precioElemento = item.getElementsByClassName('carrito-item-precio')[0];
         let precio = parseInt(precioElemento.innerText.replace('$', '').replace(' CLP', ''));
-        let cantidadItem = document.getElementsByClassName('carrito-dias-total')[0];
-        console.log(precio);
-        let cantidad = cantidadItem.value;
-        total = total + (precio * cantidad);
+        let cantidadItemDias = document.getElementsByClassName('carrito-dias-total')[0];
+        let cantidadDias = cantidadItemDias.value;
+        total = total + (precio * cantidadDias);
     }
-    total = Math.round(total * 100) / 100;
-
     document.getElementsByClassName('carrito-precio-total')[0].innerText = '$' + total + " CLP";
 
 }
